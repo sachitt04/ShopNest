3f7bd26ab60da752fed0d07aa8e5d095779fd49d
sachit <sachitforsocials@gmail.com>
2024-09-06 17:42:10 +0530

    create mixin

--- ShopNest-Dashboard/src/App.tsx
+++ ShopNest-Dashboard/src/App.tsx
@@ -1,17 +1,34 @@
- import React from 'react'
+ import React, { Suspense } from 'react'
  import { BrowserRouter as Router, Routes, Route} 
  from 'react-router-dom'
  import { lazy } from 'react'
- const Dashboard = lazy(()=> import('./pages/Dashboard'))
- 
+ import Loader from './components/Loader'
+
+const Dashboard = lazy(()=> import('./pages/Dashboard'))
+const Products = lazy(()=> import('./pages/Products'))
+const Transaction = lazy(()=> import('./pages/Transaction'))
+const Customers = lazy(()=> import('./pages/Customers'))
+
+
  
  const App = () => {
    return (
       <Router>
+      <Suspense fallback = {<Loader/>}>
         <Routes>
           <Route path='/admin/dashboard' element={<Dashboard/>} />
+          <Route path='/admin/products' element={<Products/>} />
+          <Route path='/admin/transaction' element={<Transaction/>} />
+          <Route path='/admin/customers' element={<Customers/>} />
+          
+          {/* charts */}
+
+          
+
+
 
         </Routes>
+      </Suspense>  
       </Router>
    )
  }
