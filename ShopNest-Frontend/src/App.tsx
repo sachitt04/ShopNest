0cc6acfaf307aa8676acc7e7f435b1463d9e5fa3
sachit <sachitforsocials@gmail.com>
2024-09-06 11:57:20 +0530

    first commit

---
+++ ShopNest-Frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router ,Routes ,Route } from "react-router-dom"
+import { lazy , Suspense} from "react"
+import Loader from "./components/Loader";
+
+const Home = lazy(() => import ("./pages/Home"));
+const Search = lazy(() => import("./pages/Search"))
+const Cart = lazy(() => import("./pages/Cart"))
+
+
+function App() {
+   
+
+  return (
+    <Router>
+      <Suspense fallback={<Loader/>} >
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/search" element={<Search/>} />
+        <Route path="/cart" element={<Cart/>} />
+      </Routes>
+      </Suspense>
+
+
+    </Router>
+ 
+  )
+}
+
+export default App
